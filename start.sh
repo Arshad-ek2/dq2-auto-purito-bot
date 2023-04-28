cb87b563416808981e27b9bbbfe28f05c4a0a592
Arshad-ek2 <130440778+Arshad-ek2@users.noreply.github.com>
2023-04-28 10:26:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Arshad-ek2/dq2-auto-purito-bot /dq2-auto-purito-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /dq2-auto-purito-bot
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
